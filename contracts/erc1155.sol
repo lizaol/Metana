c1184fa2dbe4d6989ae55111125551b9ecc2f824
lizaol <91904895+lizaol@users.noreply.github.com>
2023-03-05 09:43:58 -0500

    Update erc1155.sol

--- contracts/erc1155.sol
+++ contracts/erc1155.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract erc1155 is ERC1155, Ownable, ERC1155Burnable {
     uint256 public constant GOLD = 0;
@@ -17,5 +17,8 @@ contract erc1155 is ERC1155, Ownable, ERC1155Burnable {
     constructor() ERC1155("https://gateway.pinata.cloud/ipfs/QmYXHrLLax44SpNbdjf6MiwK1bYTCdx9Wug4hdAUjZB26X?_gl=1*1wju8jh*_ga*MTAzODYxMTc4Mi4xNjc3MzU0NDEw*_ga_5RMPXG14TE*MTY3NzkzNjU5Ny43LjAuMTY3NzkzNjU5Ny42MC4wLjA/{id}.json") {
     }
 
+    function mint(address to, uint256 id, uint256 amount) private{
+        _mint(to, id, amount, '');
+    }
 }
 
