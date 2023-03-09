fd9113450b7bf1f98e5e55318140a14e16f36779
lizaol <91904895+lizaol@users.noreply.github.com>
2023-03-08 22:54:40 -0500

    Update forge.sol

--- contracts/forge.sol
+++ contracts/forge.sol
@@ -1,3 +1,6 @@
+// 0xc70cB09A4f01303B9Fd1F539B84d22e61c00431e               ERC1155 address
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
@@ -5,9 +8,15 @@ import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
 import "./erc1155.sol";
 
-contract forge is ERC1155Holder, ERC1155Burnable, erc1155 {
+contract forge is ERC1155Holder {
+    erc1155 token;
+    constructor(erc1155 _token){
+        token = _token;
+    }
+
     uint256 public constant GOLD = 0;
     uint256 public constant SILVER = 1;
     uint256 public constant THORS_HAMMER = 2;
@@ -17,76 +26,99 @@ contract forge is ERC1155Holder, ERC1155Burnable, erc1155 {
     uint256 public constant POTION = 6;
 
     uint forgeTime = block.timestamp;
+    mapping(address => uint256) timing;
 
+    function name() public pure returns(string memory){
+        return "forge";
+    }
+
+    function balanceOfGold() public view returns(uint){
+        return (token.balanceOf(msg.sender, GOLD));
+    }
+
+    function balanceOfSilver() public view returns(uint){
+        return (token.balanceOf(msg.sender, SILVER));
+    }
+
+
+    // Don't know how to implement in React
+    // function balanceOfBatch() public view returns(uint[] memory){
+    //     address[] memory adr = new address[](2);
+    //     uint[] memory ids = new uint[](2);
+    //     adr[0] = msg.sender;
+    //     adr[1] = msg.sender;
+    //     ids[0] = GOLD;
+    //     ids[1] = SILVER;
+    //     return token.balanceOfBatch(adr, ids);
+    // }
 
     function forging(address to, uint256 id, uint256 amount) public{
         if (id == GOLD || id == SILVER || id == THORS_HAMMER){
             require(block.timestamp > (forgeTime + 60 seconds), "No enought time past");
-            erc1155.mint(to, id, amount);
+            token.mint(to, id, amount);
 
         } else if (id == SWORD){
-            require(balanceOf(msg.sender, GOLD) > 0, "You don't have gold");
-            require(balanceOf(msg.sender, SILVER) > 0, "You don't have silver");
-            // _burnBatch(to, [GOLD, SILVER], burningPrice);
-            erc1155.burning(to, GOLD, 1);
-            erc1155.burning(to, SILVER,1);
-            erc1155.mint(to, SWORD, amount);
+            require(token.balanceOf(msg.sender, GOLD) > 0, "You don't have gold");
+            require(token.balanceOf(msg.sender, SILVER) > 0, "You don't have silver");
+            token.burning(to, GOLD, 1);
+            token.burning(to, SILVER,1);
+            token.mint(to, SWORD, amount);
 
         } else if (id == SHIELD){
-            require(balanceOf(msg.sender, THORS_HAMMER) > 0, "You don't have gold");
-            require(balanceOf(msg.sender, SILVER) > 0, "You don't have silver");
+            require(token.balanceOf(msg.sender, THORS_HAMMER) > 0, "You don't have gold");
+            require(token.balanceOf(msg.sender, SILVER) > 0, "You don't have silver");
             uint[] memory arr = new uint[](2);
             uint[] memory price = new uint[](2);
             arr[0] = GOLD;
             arr[1] = SILVER;
             price[0] = 1;
             price[1] = 1;
-            erc1155.burningBatch(to, arr, price);
-            erc1155.mint(to, SHIELD, amount);
+            token.burningBatch(to, arr, price);
+            token.mint(to, SHIELD, amount);
 
         } else if (id == LOKIS_HORNS){
-            require(balanceOf(msg.sender, GOLD) > 0, "You don't have gold");
-            require(balanceOf(msg.sender, THORS_HAMMER) > 0, "You don't have silver");
-            erc1155.burning(to, GOLD, 1);
-            erc1155.burning(to, THORS_HAMMER,1);
-            erc1155.mint(to, LOKIS_HORNS, amount);
+            require(token.balanceOf(msg.sender, GOLD) > 0, "You don't have gold");
+            require(token.balanceOf(msg.sender, THORS_HAMMER) > 0, "You don't have silver");
+            token.burning(to, GOLD, 1);
+            token.burning(to, THORS_HAMMER,1);
+            token.mint(to, LOKIS_HORNS, amount);
 
         } else if (id == POTION){
-            require(balanceOf(msg.sender, GOLD) > 0, "You don't have gold");
-            require(balanceOf(msg.sender, SILVER) > 0, "You don't have silver");
-            require(balanceOf(msg.sender, THORS_HAMMER) > 0, "You don't have silver");
-            erc1155.burning(to, GOLD, 1);
-            erc1155.burning(to, SILVER,1);
-            erc1155.burning(to, THORS_HAMMER,1);
-            erc1155.mint(to, POTION, amount);
+            require(token.balanceOf(msg.sender, GOLD) > 0, "You don't have gold");
+            require(token.balanceOf(msg.sender, SILVER) > 0, "You don't have silver");
+            require(token.balanceOf(msg.sender, THORS_HAMMER) > 0, "You don't have silver");
+            token.burning(to, GOLD, 1);
+            token.burning(to, SILVER,1);
+            token.burning(to, THORS_HAMMER,1);
+            token.mint(to, POTION, amount);
         }
     }
 
     function trade(address account, uint256 id, uint256 amount) public{
         if(id == SHIELD || id == LOKIS_HORNS || id == POTION){
-            require(balanceOf(msg.sender, SHIELD) > amount, "You don't have shields");
-            require(balanceOf(msg.sender, LOKIS_HORNS) > amount, "You don't have loki's horns");
-            require(balanceOf(msg.sender, POTION) > amount, "You don't have potion");
-            erc1155.burning(account, id, amount);
+            require(token.balanceOf(msg.sender, SHIELD) > amount, "You don't have shields");
+            require(token.balanceOf(msg.sender, LOKIS_HORNS) > amount, "You don't have loki's horns");
+            require(token.balanceOf(msg.sender, POTION) > amount, "You don't have potion");
+            token.burning(account, id, amount);
         } else {
             if(id == GOLD) {
-                require(balanceOf(msg.sender, GOLD) > 1, "You don't have gold");
-                erc1155.burning(account, GOLD, 1);
-                erc1155.mint(account, SILVER, 2);
+                require(token.balanceOf(msg.sender, GOLD) > 1, "You don't have gold");
+                token.burning(account, GOLD, 1);
+                token.mint(account, SILVER, 2);
             } else if (id == SILVER){
-                require(balanceOf(msg.sender, SILVER) > 1, "You don't have silver");
-                erc1155.burning(account, SILVER, 1);
-                erc1155.mint(account, THORS_HAMMER, 1);
+                require(token.balanceOf(msg.sender, SILVER) > 1, "You don't have silver");
+                token.burning(account, SILVER, 1);
+                token.mint(account, THORS_HAMMER, 1);
             } else {
-                require(balanceOf(msg.sender, THORS_HAMMER) > 1, "You don't have gold");
-                erc1155.burning(account, THORS_HAMMER, 1);
-                erc1155.mint(account, SILVER, 2);
-                erc1155.mint(account, GOLD, 2);
+                require(token.balanceOf(msg.sender, THORS_HAMMER) > 1, "You don't have gold");
+                token.burning(account, THORS_HAMMER, 1);
+                token.mint(account, SILVER, 2);
+                token.mint(account, GOLD, 2);
             }
         }
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC1155Receiver) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155Receiver) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
     function onERC1155Received(
@@ -109,3 +141,4 @@ contract forge is ERC1155Holder, ERC1155Burnable, erc1155 {
     }
 
 }
+
