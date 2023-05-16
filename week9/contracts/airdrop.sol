ade5d5b62cc933191af3e39b25c3930e12728f81
Elizaveta Olenberg <eolenberg@drew.edu>
2023-05-16 16:47:02 -0400

    week9

--- week9/contracts/airdrop.sol
+++ week9/contracts/airdrop.sol
@@ -1,27 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
-import "./merkleToken.sol";
+// import "@openzeppelin/contracts/utils/Multicall.sol";
 
-contract airDrop {
+
+contract MerkleToken is ERC721, Ownable {
     using BitMaps for BitMaps.BitMap;
-    MerkleToken token;
+    BitMaps.BitMap private claimed;
     bytes32 public immutable root;
     uint public immutable rewardAmount;
-    BitMaps.BitMap private claimed;
+    
+    struct Id{              
+        bytes32 hash;
+        uint blockNum;
+        uint nftID;
+    }
 
-    constructor(bytes32 _root, uint _amount, address _token){
+    enum Stage{
+        Presale,
+        PublicSale,
+        SupplyOut
+    }
+    Stage public stage;
+    mapping(address => Id) public commits;
+    mapping(address => bool) public hasMinted;
+
+    constructor(bytes32 _root, uint _amount) ERC721("Merkle", "MER") {
         root = _root;
         rewardAmount = _amount;
-        token = MerkleToken(_token);
+        stage = Stage.Presale;
     }
-    
-    function claim(bytes32[] calldata proof, uint id, string memory secret) external{
-        // require();
-        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-        require(MerkleProof.verify(proof, root, leaf), "Incorrect merkle proof");
-        token.reveal(id, secret);
+
+    function transferMultipleNFT(uint256[] memory tokenIds, address[] memory to) public {
+        require(tokenIds.length == to.length, "tokenIds length doesnt match to length");
+        for(uint i=0; i<to.length; i++){
+            require(ownerOf(tokenIds[i]) == msg.sender, "Sender does not own this NFT");
+            safeTransferFrom(msg.sender, to[i], tokenIds[i]);
+        }
     }
+
+    // commit to a value while keeping it hidden with the ability to reveal it later
+    function submitCommit(uint number, string memory secret) public {
+        require(stage == Stage.Presale, "presale is over");
+        bytes32 numHash = bytes32(keccak256(abi.encodePacked(number, secret)));
+        require(commits[msg.sender].hash != numHash, "Commitment already submitted");
+        commits[msg.sender] = Id(numHash, block.number, 0);                                 // ????
+    }
+
+    function reveal(uint number, string memory secret) public {
+        require(stage = Stage.PublicSale, "Public sale hasn't started yet /or it's over");
+        Id storage commit = commits[msg.sender];
+        // require(!commit.revealed, "Already revealed");
+        require(!hasMinted[msg.sender], "Already revealed");
+        require(commit.hash == keccak256(abi.encodePacked(number, secret)), "Invalid reveal");
+        require(block.number > commit.blockNum + 10, "Reveal too early");
+        uint tokenId = uint256(keccak256(abi.encodePacked(number, secret, blockhash(commit.blockNum + 10))));
+        hasMinted[msg.sender] = true;
+        commit.nftID = tokenId;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function nextStage() public onlyOwner{
+        require(stage != Stage.SupplyOut, "you're at the last stage");
+        stage = Stage(uint(stage) + 1);         //casting: enum members are assigned consecutive integers starting from 0
+    } 
+
 }
+ 
--- week9/contracts/merkleToken.sol
+++ week9/contracts/merkleToken.sol
@@ -3,26 +3,34 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
 
-contract MerkleToken is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+
+contract MerkleToken is ERC721, Ownable, Multicall {
+    using BitMaps for BitMaps.BitMap;
+    
+    MerkleToken token;
+    bytes32 public immutable root;
+    uint public immutable rewardAmount;
+    BitMaps.BitMap private claimed;
     struct Id{
         bytes32 hash;
         uint blockNum;
         // bool revealed;
     }
     mapping(address => Id) public commits;
-    mapping(address => bool) public minted;
+    mapping(address => bool) public hasMinted;
 
-    constructor() ERC721("Merkle", "MER") {}
+    constructor(bytes32 _root, uint _amount) ERC721("Merkle", "MER") {
+        root = _root;
+        rewardAmount = _amount;
+    }
 
-    // function safeMint(address to) private onlyOwner {
-    //     uint256 tokenId = _tokenIdCounter.current();
-    //     _tokenIdCounter.increment();
-    //     _safeMint(to, tokenId);
-    // }
+    function transferMultipleNFT(uint256[] memory tokenIds, address[] memory to) public {
+        require(msg.sender == );
+    }
 
     // commit to a value while keeping it hidden with the ability to reveal it later
     function submitCommit(uint number, string memory secret) public {
@@ -34,11 +42,11 @@ contract MerkleToken is ERC721, Ownable {
     function reveal(uint number, string memory secret) public {
         Id storage commit = commits[msg.sender];
         // require(!commit.revealed, "Already revealed");
-        require(!minted[msg.sender], "Already revealed");
+        require(!hasMinted[msg.sender], "Already revealed");
         require(commit.hash == keccak256(abi.encodePacked(number, secret)), "Invalid reveal");
         require(block.number > commit.blockNum + 10, "Reveal too early");
         uint tokenId = uint256(keccak256(abi.encodePacked(number, secret, blockhash(commit.blockNum + 10))));
-        minted[msg.sender] = true;
+        hasMinted[msg.sender] = true;
         _safeMint(msg.sender, tokenId);
     }
 
